a050484288c58578515d4a5b82242a969444814a
Eric Lamb <eric.lamb@softekinc.com>
2021-03-15 12:09:07 -0400

    Opening the web page.
    We are using [Protractor](https://www.nuget.org/packages/Protractor/0.13.0) because we are testing an Angular application. Protractor will wait for Angular to complete its operations. Using Selenium on its own could result in tests working inconsistently.
    
    We are using [Selenium.WebDriver.ChromeDriver](https://www.nuget.org/packages/Selenium.WebDriver.ChromeDriver/) so we can control the Chrome browser. Controlling other browsers will require other drivers.
    
    We are using the `--ignore-certificate-errors` to ensure tests run even if ASP.Net's development certificate is untrusted.
    
    Using statements are necessary to terminate child processes.
    
    At this point running the test should open a Chrome web browser. The home page will appear for a moment before the browser is closed.

--- WebAppTesting.Test/UnitTest1.cs
+++ WebAppTesting.Test/UnitTest1.cs
@@ -1,4 +1,9 @@
 using System;
+using System.IO;
+using OpenQA.Selenium.Chrome;
+using Protractor;
+using WebAppTesting.Test.Infrastructure;
+using WebAppTesting.Web;
 using Xunit;
 
 namespace WebAppTesting.Test
@@ -8,7 +13,17 @@ namespace WebAppTesting.Test
         [Fact]
         public void Test1()
         {
+            using var webApplicationFactory = new SeleniumServerFactory<Startup>();
+            using var client = webApplicationFactory.CreateClient();
 
+            var options = new ChromeOptions();
+            options.AddArgument("--ignore-certificate-errors");
+
+            using var driver = new ChromeDriver(Path.GetFullPath("."), options);
+            driver.Manage().Timeouts().AsynchronousJavaScript = TimeSpan.FromSeconds(30);
+            using var ngDriver = new NgWebDriver(driver);
+
+            ngDriver.Url = webApplicationFactory.RootUri;
         }
     }
 }
